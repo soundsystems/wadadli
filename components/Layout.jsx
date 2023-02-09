99f9d10a7efa52454fd04bddd551bb625ce8200f
soundsystems <jaay.g@disroot.org>
2023-02-09 17:19:17 -0600

    Components

---
+++ components/Layout.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import TransitionEffect1 from "./TransitionEffect1";
+
+const Layout = ({ children }) => (
+<TransitionEffect1>
+{children}
+</TransitionEffect1>
+);
+
+export default Layout
